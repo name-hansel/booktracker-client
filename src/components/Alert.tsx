91adf52526adfa3912a66f78c01500f299861402
name-hansel <snehal6mane@gmail.com>
2021-07-27 20:02:28 +0530

    Added new spinner

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 
 import { Alert as AlertInterface } from "../interfaces";
+import AlertContext from "../context/alert";
 
 interface Props {
   alerts: AlertInterface[];
 }
 
 const Alert = ({ alerts }: Props) => {
+  const { alertDispatch } = React.useContext(AlertContext);
+
   return (
     <div className="alerts">
       {alerts && alerts.length > 0 ? (
         alerts.map((alert) => (
           <div key={alert.id} className={`alert ${alert.type}`}>
             {alert.text}
-            {/* Add button to remove alert */}
+            <button
+              onClick={(e) =>
+                alertDispatch({
+                  type: "REMOVE_ALERT",
+                  payload: alert.id,
+                })
+              }
+            ></button>
           </div>
         ))
       ) : (
